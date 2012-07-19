0844730916e2c85324ea7bd49d0b2d1006a25a36
Agus Sugianto <agus.delonge@gmail.com>
2012-07-19 15:41:08 +0700

    update script 29_structure_data.sql * add report for laporan performance RCA * add report for laporan partisipasi STOP
    Signed-off-by: Agus Sugianto <agus.delonge@gmail.com>

--- db/29_structure_data.sql
+++ db/29_structure_data.sql
@@ -115,3 +115,57 @@ values (
 ,	'IS_IN_ORG;YEAR;MONTH;ID_DIR;ID_DIV;ID_DEP;ID_DINAS;ID_SEKSI;ID_WILAYAH;ID_AREA'
 )
 go
+
+insert into R_REPORT (
+	NAMA_REPORT
+,	KLASIFIKASI_REPORT
+,	TIPE_REPORT
+,	FILE_LOCATION
+,	WEB_PARAMS
+,	REPORT_PARAMS
+)
+values (
+	'Laporan Performance RCA'
+,	5
+,	'xls'
+,	'reports/rca_lap_performance.jasper'
+,	'is_in_org;id_dir;id_div;id_dep;id_dinas;id_seksi;id_wilayah;id_area;year;month'
+,	'IS_IN_ORG;ID_DIR;ID_DIV;ID_DEP;ID_DINAS;ID_SEKSI;ID_WILAYAH;ID_AREA;YEAR;MONTH'
+)
+go
+
+insert into R_REPORT (
+	NAMA_REPORT
+,	KLASIFIKASI_REPORT
+,	TIPE_REPORT
+,	FILE_LOCATION
+,	WEB_PARAMS
+,	REPORT_PARAMS
+)
+values (
+	'Laporan Partisipasi STOP Pegawai'
+,	5
+,	'xls'
+,	'reports/obs_lap_partisipasi_pegawai.jasper'
+,	'is_in_org;id_dir;id_div;id_dep;id_dinas;id_seksi;id_wilayah;id_area;year'
+,	'IS_IN_ORG;ID_DIR;ID_DIV;ID_DEP;ID_DINAS;ID_SEKSI;ID_WILAYAH;ID_AREA;YEAR'
+)
+go
+
+insert into R_REPORT (
+	NAMA_REPORT
+,	KLASIFIKASI_REPORT
+,	TIPE_REPORT
+,	FILE_LOCATION
+,	WEB_PARAMS
+,	REPORT_PARAMS
+)
+values (
+	'Laporan Partisipasi STOP Organisasi'
+,	5
+,	'xls'
+,	'reports/obs_lap_partisipasi_organisasi.jasper'
+,	'is_in_org;id_dir;id_div;id_dep;id_dinas;id_seksi;id_wilayah;id_area;year;month'
+,	'IS_IN_ORG;ID_DIR;ID_DIV;ID_DEP;ID_DINAS;ID_SEKSI;ID_WILAYAH;ID_AREA;YEAR;MONTH'
+)
+go
--- module/rca_lap_performance/layout.js
+++ module/rca_lap_performance/layout.js
@@ -219,23 +219,120 @@ function M_RCALapRCAGrid()
 		singleSelect	: true
 	});
 
-	this.panel = new Ext.grid.GridPanel({
-		title				: 'RCA Tabel'
-	,	store				: this.store
-	,	cm					: this.cm
-	,	autoHeight			: true
-	,	autoScroll			: true
-	,	autoExpandColumn	: 'item'
-	,	viewConfig			: {forceFit: true}
-	,	tbar		: [
-			{
-				xtype	: 'exportbutton'
-			,	store	: this.store
+	this.btn_print = new Ext.Button({
+			text		: 'Print'
+		,	iconCls		: 'print16'
+		,	scope		: this
+		,	handler		: function() {
+				this.do_print();
 			}
-		]
 	});
 
+	this.panel = new Ext.grid.GridPanel({
+			title				: 'RCA Tabel'
+		,	store				: this.store
+		,	cm					: this.cm
+		,	autoHeight			: true
+		,	autoScroll			: true
+		,	autoExpandColumn	: 'item'
+		,	viewConfig			: {forceFit: true}
+		,	tbar				: [ this.btn_print]
+	});
 
+	this.do_print = function()
+	{
+		var form;
+		var id_report	= '26';
+		var tipe_report	= 'xls';
+		var isinorg		= m_rca_lap_performance.form.set_org.isChecked();
+		
+		if (isinorg){
+			is_in_org	= '1'
+		} else {
+			is_in_org	= '0'
+		}
+		
+		form = document.createElement('form');
+
+		form.setAttribute('method', 'post');
+		form.setAttribute('target', '_blank');		
+		form.setAttribute('action', _g_root +'/report');
+		
+		var hiddenField1 = document.createElement ('input');
+        hiddenField1.setAttribute('type', 'hidden');
+        hiddenField1.setAttribute('name', 'id');
+        hiddenField1.setAttribute('value', id_report);
+		
+		var hiddenField2 = document.createElement ('input');
+        hiddenField1.setAttribute('type', 'hidden');
+		hiddenField2.setAttribute('name', 'type');
+        hiddenField2.setAttribute('value', tipe_report);
+
+		var hiddenField3 = document.createElement ('input');
+        hiddenField1.setAttribute('type', 'hidden');
+		hiddenField3.setAttribute('name', 'is_in_org');
+        hiddenField3.setAttribute('value', is_in_org);
+		
+		var hiddenField4 = document.createElement ('input');
+        hiddenField1.setAttribute('type', 'hidden');
+		hiddenField4.setAttribute('name', 'id_dir');
+        hiddenField4.setAttribute('value', m_rca_lap_performance.form.set_org.formDirektorat.getValue ());
+		
+		var hiddenField5 = document.createElement ('input');
+        hiddenField1.setAttribute('type', 'hidden');
+		hiddenField5.setAttribute('name', 'id_div');
+        hiddenField5.setAttribute('value', m_rca_lap_performance.form.set_org.formDivProSBU.getValue ());
+		
+		var hiddenField6 = document.createElement ('input');
+        hiddenField1.setAttribute('type', 'hidden');
+		hiddenField6.setAttribute('name', 'id_dep');
+        hiddenField6.setAttribute('value', m_rca_lap_performance.form.set_org.formDepartemen.getValue());
+		
+		var hiddenField7 = document.createElement ('input');
+        hiddenField1.setAttribute('type', 'hidden');
+		hiddenField7.setAttribute('name', 'id_dinas');
+        hiddenField7.setAttribute('value', m_rca_lap_performance.form.set_org.formDinas.getValue());
+		
+		var hiddenField8 = document.createElement ('input');
+        hiddenField1.setAttribute('type', 'hidden');
+		hiddenField8.setAttribute('name', 'id_seksi');
+        hiddenField8.setAttribute('value', m_rca_lap_performance.form.set_org.formSeksi.getValue());
+		
+		var hiddenField9 = document.createElement ('input');
+        hiddenField1.setAttribute('type', 'hidden');
+		hiddenField9.setAttribute('name', 'id_wilayah');
+        hiddenField9.setAttribute('value', m_rca_lap_performance.form.set_wil.formWilayah.getValue());
+		
+		var hiddenField10 = document.createElement ('input');
+        hiddenField1.setAttribute('type', 'hidden');
+		hiddenField10.setAttribute('name', 'id_area');
+        hiddenField10.setAttribute('value', m_rca_lap_performance.form.set_wil.formArea.getValue());
+		
+		var hiddenField11 = document.createElement ('input');
+        hiddenField1.setAttribute('type', 'hidden');
+		hiddenField11.setAttribute('name', 'year');
+        hiddenField11.setAttribute('value', m_rca_lap_performance.form.set_waktu.formTahun.getValue());
+		
+		var hiddenField12 = document.createElement ('input');
+        hiddenField1.setAttribute('type', 'hidden');
+		hiddenField12.setAttribute('name', 'month');
+        hiddenField12.setAttribute('value', m_rca_lap_performance.form.set_waktu.formBulan.getValue());
+		
+		form.appendChild(hiddenField1);
+		form.appendChild(hiddenField2);
+		form.appendChild(hiddenField3);
+		form.appendChild(hiddenField4);
+		form.appendChild(hiddenField5);
+		form.appendChild(hiddenField6);
+		form.appendChild(hiddenField7);
+		form.appendChild(hiddenField8);
+		form.appendChild(hiddenField9);
+		form.appendChild(hiddenField10);
+		form.appendChild(hiddenField11);
+		form.appendChild(hiddenField12);
+		document.body.appendChild(form);
+		form.submit();
+	}
 }
 
 function M_RCALapperformance()
