5b6f924111f54779f10b849f140e2075df03bf2d
Agus Sugianto <agus.delonge@gmail.com>
2012-09-28 10:59:14 +0700

    db: update script 36_structure.sql * db: update script k3pl.sql
    Signed-off-by: Agus Sugianto <agus.delonge@gmail.com>

--- db/36_structure.sql
+++ db/36_structure.sql
@@ -1,10 +1,24 @@
-/* table of RCA images */
-drop table T_RCA_IMAGES;
-
 create table T_RCA_IMAGES (
-	ID_RCA		bigint			foreign key references T_RCA (ID_RCA)
+	ID_RCA		bigint			not null
 ,	IMAGE_NAME	varchar (1024)	not null
 ,	IMAGE_SIZE	int				not null
-,	UPLOAD_DATE	datetime		not null default current_timestamp
-,	UPLOADER	varchar (20)	foreign key references __USER (NIPG)
-);
+,	UPLOAD_DATE	datetime		not null default getdate()
+,	UPLOADER	varchar (20)	not null
+,	constraint PK_T_RCA_IMAGES primary key (ID_RCA)
+)
+go
+
+create index T_RCA_IMAGES_FK on T_RCA_IMAGES (
+UPLOADER ASC
+)
+go
+
+alter table T_RCA_IMAGES
+   add constraint FK_T_RCA_T_RCA_IMAGES foreign key (ID_RCA)
+      references T_RCA (ID_RCA)
+go
+
+alter table T_RCA_IMAGES
+   add constraint FK___USER_T_RCA_IMAGES foreign key (UPLOADER)
+      references __USER (NIPG)
+go
--- db/k3pl.sql
+++ db/k3pl.sql
@@ -17593,3 +17593,28 @@ insert into R_CSM_PENILAIAN2_PENILAIAN	values (10, 15, 75, 'Tidak', 0);
 insert into R_CSM_PENILAIAN2_DETAIL2	values (10, 15, 'Apakah Perusahaan menetapkan maksimum waktu perbaikan atas temuan audit < 2 minggu?');
 insert into R_CSM_PENILAIAN2_PENILAIAN	values (10, 15, 76, 'Ya (Lampirkan)', 1);
 insert into R_CSM_PENILAIAN2_PENILAIAN	values (10, 15, 76, 'Tidak', 0);
+
+create table T_RCA_IMAGES (
+	ID_RCA		bigint			not null
+,	IMAGE_NAME	varchar (1024)	not null
+,	IMAGE_SIZE	int				not null
+,	UPLOAD_DATE	datetime		not null default getdate()
+,	UPLOADER	varchar (20)	not null
+,	constraint PK_T_RCA_IMAGES primary key (ID_RCA)
+)
+go
+
+create index T_RCA_IMAGES_FK on T_RCA_IMAGES (
+UPLOADER ASC
+)
+go
+
+alter table T_RCA_IMAGES
+   add constraint FK_T_RCA_T_RCA_IMAGES foreign key (ID_RCA)
+      references T_RCA (ID_RCA)
+go
+
+alter table T_RCA_IMAGES
+   add constraint FK___USER_T_RCA_IMAGES foreign key (UPLOADER)
+      references __USER (NIPG)
+go
